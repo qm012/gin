3b84f60b7b2b341d3a9c1ac689167333590322f3
qm012 <urobot@qq.com>
2021-07-05 13:11:09 +0800

    vtest23

--- tree.go
+++ tree.go
@@ -446,7 +446,7 @@ walk: // Outer loop for walking the tree
 						n = n.children[i]
 
 						// match '/', If this condition is matched, the next route is found
-						if (len(n.fullPath) != 0 && n.wildChild) || strings.HasSuffix(path, "/") {
+						if strings.HasSuffix(path, "/") || (len(n.fullPath) != 0 && n.wildChild) {
 							matchNum++
 						}
 						continue walk
